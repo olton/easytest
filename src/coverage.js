034013e17cfc106a0f871b9ee4bbf1424880e4ae
olton <serhii@pimenov.com.ua>
2024-09-27 23:47:42 +0300

    improved coverage table

--- src/coverage.js
+++ src/coverage.js
@@ -45,25 +45,41 @@ const generateReport = (filename, sourceCode, functions) => {
     return [
         _fileName,
         complete ? chalk.green(_baseName) : chalk.red(_baseName),
-        (complete ? chalk.green(_coveredLinesPercent) : chalk.red(_coveredLinesPercent)) + " %"
+        (complete ? chalk.green(_coveredLinesPercent) : chalk.red(_coveredLinesPercent)),
+        totalLines,
+        coveredLines,
     ]
 }
 
 export function createReport (coverage, root) {
     const data = []
 
+    let totalLines = 0
+    let coveredLines = 0
+    let totalFiles = 0
+
+    data.push(["#", "File Path", "File Name", "Lines Total", "Covered Lines", '%'])
+
     coverageFilter(coverage, root).map(({url, functions}) => {
         const fileName = fileURLToPath(url)
         const sourceCode = fs.readFileSync(fileName, 'utf-8')
 
-        data.push(generateReport(fileName.replace(root, ''), sourceCode, functions))
+        const [_path, _name, _percent, _total, _covered] = generateReport(fileName.replace(root, ''), sourceCode, functions)
+
+        totalLines += _total
+        coveredLines += _covered
+        totalFiles++
+
+        data.push([totalFiles, _path, _name, _total, _covered, _percent + ' %'])
     })
 
     const config = {
-        singleLine: true,
         columns: [
             {alignment: 'left'},
             {alignment: 'left'},
+            {alignment: 'left'},
+            {alignment: 'right'},
+            {alignment: 'right'},
             {alignment: 'right', width: 10},
         ],
         border: {
@@ -85,10 +101,20 @@ export function createReport (coverage, root) {
             joinLeft: `├`,
             joinRight: `┤`,
             joinJoin: `┼`
+        },
+        drawHorizontalLine: (lineIndex, rowCount) => {
+            return lineIndex === 0 || lineIndex === 1 || lineIndex === rowCount;
         }
     };
 
     log(`\nCoverage report for:`)
     log(chalk.yellow.bold(root))
+    log(`------------------------------------`)
+    log(`Files:          ${chalk.magenta.bold(totalFiles)}`)
+    log(`Lines of code:  ${chalk.blue.bold(totalLines)}`)
+    log(`Covered Lines:  ${chalk.green.bold(coveredLines)}`)
+    let totalProgress = Math.round(coveredLines * 100 / totalLines)
+    log(`Total coverage: ${totalProgress < 50 ? chalk.red.bold(totalProgress) : totalProgress < 80 ? chalk.yellow.bold(totalProgress) : chalk.green.bold(totalProgress) } %`)
+    log(`------------------------------------`)
     log(table(data, config))
 }
