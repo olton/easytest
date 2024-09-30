74e6ff5671c92ef8396f1fd9bf10bddb55a65f8e
olton <serhii@pimenov.com.ua>
2024-10-01 01:35:51 +0300

    add extend demo

---
+++ __tests__/extend.test.js
@@ -0,0 +1,17 @@
+import {Expect} from "../src/expect.js";
+
+class MyExpect extends Expect {
+    toBeEven() {
+        let received = this.received
+        let result = received % 2 === 0
+        if (!result) {
+            this.message = `Expected ${received} to be even`
+        }
+    }
+}
+
+const expect = (received) => new MyExpect(received)
+
+test(`Custom expect`, () => {
+    expect(2).toBeEven()
+})
